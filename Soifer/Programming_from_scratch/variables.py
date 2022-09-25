17e216a224fa88fae22ca018fc47a10f2109be0e
Juan Pablo Rodriguez <juanprodriguez.mail@gmail.com>
2022-09-25 13:23:53 -0300

    Variables

---
+++ Soifer/Programming_from_scratch/variables.py
@@ -0,0 +1,18 @@
+nombre = 'JP'
+print(nombre)
+nombre = "Rodriguez"
+print(nombre)
+edad = 44
+print(edad - 5)
+print(edad + 8)
+print(edad * 2)
+print(edad / 2)
+print(edad ** 2)
+print(edad % 3)
+
+estatura = 1.70
+#estatura_doble = 1.70 * 2
+#print(estatura_doble)
+
+estatura = estatura * 2
+print(estatura)
