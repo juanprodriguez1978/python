f1a3db3e6794e3756c92075d14f971daa5397784
Juan Pablo Rodriguez <juanprodriguez.mail@gmail.com>
2022-10-02 09:16:43 -0300

    Class 3

---
+++ Soifer/Programming_from_scratch/datos.py
@@ -0,0 +1,8 @@
+nombre = input('Ingrese su nombre: ')
+edad = int(input('Ingrese su edad: '))
+
+if  nombre == 'Smith' :
+    print('Amigo Smith, puede ingresar.')
+
+if edad >= 18:
+    print('Usted es mayor de edad.')
---
+++ Soifer/Programming_from_scratch/dicidir.py
@@ -0,0 +1,21 @@
+# >  mayor
+# <  menor
+# >= mayor o igual 
+# <= menor o igual
+# == Verificacion de igualdad
+
+mayor = True
+menor = False
+
+print(mayor)
+print(menor)
+
+a = 18 + 2 > 10
+print(a)
+print(18 + 2 > 10)
+
+print(30*2 == 60)
+
+
+
+
