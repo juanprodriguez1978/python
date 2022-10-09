cb1f8657a26a6f2b9681865a7417daa29a4b4d3f
Juan Pablo Rodriguez <juanprodriguez.mail@gmail.com>
2022-10-09 15:26:06 -0300

    add parimparcero

---
+++ Soifer/challenges/parimparcero.py
@@ -0,0 +1,8 @@
+num = int(input('Ingrese un numero: '))
+
+if num == 0:
+    print('El numero ingresado es 0.')
+elif num % 2 == 0:
+    print('El numero ingresado es par.')
+else:
+    print('El numero ingresado es impar.')
