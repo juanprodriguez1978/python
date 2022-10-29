bab37c174a5e6ca15b49913889d94f46daee0ce4
Juan Pablo Rodriguez <juanprodriguez.mail@gmail.com>
2022-10-29 17:52:38 -0300

    add while.py

---
+++ Soifer/Programming_from_scratch/while.py
@@ -0,0 +1,17 @@
+# Tengo una variable que empieza en 0
+# Imprimo el valor de la variable
+# en cada iteracion, le sumo 5
+# Quiero que este loop se ejecute mientras
+# esta variable tenga un valor menor a 47
+x = 0
+while  x < 47:
+    print(x)
+    x = x + 5
+print('Ya salimos')
+
+y = ''
+while y != 'Hola':
+    y = input('Hola!!!: ')
+    
+    
+    
