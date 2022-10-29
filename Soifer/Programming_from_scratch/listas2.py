e08874245f793f8ac48f02bd7bcd97d3e6e97755
Juan Pablo Rodriguez <juanprodriguez.mail@gmail.com>
2022-10-29 18:56:20 -0300

    add listas2.py

---
+++ Soifer/Programming_from_scratch/listas2.py
@@ -0,0 +1,17 @@
+nombres = ['Jorge', 'Natalia', 'Raul', 'Clara']
+edades = [10, 15, 45, 84, 21]
+cosas = [1, 2.34, 'Ruben', True, 'J', 3.14]
+cosas_2 = [1, 2, 3, 'Ruben', ['A', 'B', 'C',[1, 2, 3, 4, 5]], nombres]
+
+# for i in nombres:
+#     print(i)
+    
+# for i in edades:
+#     print(i)
+    
+# for i in cosas:
+#     print(i)
+print(cosas_2[4][3][3])
+
+# for i in cosas_2:
+#     print(i)
